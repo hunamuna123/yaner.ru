63bcdd4c94c67ac9d7424061e5f8cd12e550cd06
hunamuna123 <95960372+hunamuna123@users.noreply.github.com>
2025-05-01 22:26:14 +0300

    y. metric

--- app.vue
+++ app.vue
@@ -1,6 +1,12 @@
 <template>
-  <NuxtPage/>
+  <NuxtLayout>
+    <NuxtPage />
+    <noscript>
+      <div><img src="https://mc.yandex.ru/watch/101540940" style="position:absolute; left:-9999px;" alt="" /></div>
+    </noscript>
+  </NuxtLayout>
 </template>
+
 <style>
 body, html {
     margin: 0;
